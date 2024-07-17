2926cc69946cae6e12931ed62decca9e00cedcf8
jalilnawawi <ajnawawi@gmail.com>
2024-07-18 02:23:11 +0700

    fix : login service

--- src/main/java/com/finalproject/finsera/finsera/controller/AuthController.java
+++ src/main/java/com/finalproject/finsera/finsera/controller/AuthController.java
@@ -2,30 +2,23 @@ package com.finalproject.finsera.finsera.controller;
 
 import com.finalproject.finsera.finsera.dto.login.LoginRequestDto;
 import com.finalproject.finsera.finsera.dto.login.LoginResponseDto;
+import com.finalproject.finsera.finsera.dto.login.ReloginRequestDto;
 import com.finalproject.finsera.finsera.dto.login.ReloginResponseDto;
 import com.finalproject.finsera.finsera.dto.register.RegisterRequestDto;
-import com.finalproject.finsera.finsera.exception.ErrorResponse;
 import com.finalproject.finsera.finsera.model.entity.Customers;
-import com.finalproject.finsera.finsera.model.enums.StatusUser;
 import com.finalproject.finsera.finsera.repository.CustomerRepository;
 import com.finalproject.finsera.finsera.service.CustomerService;
 import com.finalproject.finsera.finsera.util.JwtUtil;
-import com.finalproject.finsera.finsera.util.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("auth")
@@ -45,9 +38,6 @@ public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
 
-    @Autowired
-    ErrorResponse errorResponse;
-
     //ignore register service
     @PostMapping("/register")
     public ResponseEntity<Map<String, Object>> register(@RequestBody RegisterRequestDto registerRequestDto){
@@ -63,48 +53,20 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<Map<String, Object>> login(@RequestBody LoginRequestDto loginRequestDto){
-        try {
-            Authentication authentication = authenticationManager
-                    .authenticate(new UsernamePasswordAuthenticationToken(
-                            loginRequestDto.getUsername(),
-                            loginRequestDto.getPassword()
-                    ));
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-            String jwt = jwtUtil.generateToken(authentication);
-
-            UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-
-            Optional<Customers> optionalCustomers = customerRepository.findByUsername(userDetails.getUsername());
-            Customers customers = optionalCustomers.get();
-            if (optionalCustomers.isPresent()
-                    &&
-                    loginRequestDto.getUsername().equals(customers.getUsername())
-                    &&
-                    passwordEncoder.matches(loginRequestDto.getPassword(), customers.getPassword())
-            ){
-                customers.setStatusUser(StatusUser.ACTIVE);
-                customerRepository.save(customers);
-                Map<String, Object> response = new HashMap<>();
-                response.put("message", "Login success");
-
-                LoginResponseDto loginResponseDto = new LoginResponseDto(
-                        jwt, userDetails.getUsername(), customers.getStatusUser()
-                );
-                response.put("data", loginResponseDto);
-                return new ResponseEntity<>(response, HttpStatus.OK);
-            } else {
-                System.out.println(errorResponse);
-                return errorResponse.message("Username or Password is invalid");
-            }
-        } catch (AuthenticationException e){
-            System.out.println(e);
-            return errorResponse.message("Username or Password is invalid");
-        }
-
+        LoginResponseDto login = customerService.login(loginRequestDto);
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "Login success");
+        response.put("data", login);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @PostMapping("/relogin")
     public ResponseEntity<Map<String, Object>> relogin(Principal principal, @RequestParam String mpin){
+//        ReloginResponseDto relogin = customerService.relogin(principal);
+//        Map<String, Object> response = new HashMap<>();
+//        response.put("message", "Login success");
+//        response.put("data", relogin);
+//        return new ResponseEntity<>(response, HttpStatus.OK);
         String username = principal.getName();
         String userPin = customerService.getUserPin(username);
         Customers customers = customerRepository.findByUsername(username).get();
