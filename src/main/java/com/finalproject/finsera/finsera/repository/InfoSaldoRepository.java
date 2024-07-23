bfe600e7346e125317d7ee8a2c1d3f4f5c92d4b5
Fahrizal Shofyan Aziz <fahrizalshofyanaziz@gmail.com>
2024-07-23 20:38:15 +0700

    add : unit test

--- src/main/java/com/finalproject/finsera/finsera/repository/InfoSaldoRepository.java
+++ src/main/java/com/finalproject/finsera/finsera/repository/InfoSaldoRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
+
 public interface InfoSaldoRepository extends JpaRepository<BankAccounts, Long> {
     Optional<BankAccounts> findByAccountNumber(String accountNumber);
     Optional<BankAccounts> findByCustomer(Customers customer);
