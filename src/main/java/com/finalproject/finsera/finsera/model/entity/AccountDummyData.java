4f1e89ef080542dfc4a48876f13d4e647968cc67
jalilnawawi <ajnawawi@gmail.com>
2024-07-25 01:17:41 +0700

    build : merge development branch

---
+++ src/main/java/com/finalproject/finsera/finsera/model/entity/AccountDummyData.java
@@ -0,0 +1,11 @@
+package com.finalproject.finsera.finsera.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "account_dummy_data", schema = "public")
+public class AccountDummyData {
+}
