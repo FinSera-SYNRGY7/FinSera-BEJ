b667d58802c6b470ead25db7d8f5b27ba617a301
jalilnawawi <ajnawawi@gmail.com>
2024-08-22 23:08:19 +0700

    fix : handling request body unknown field

--- src/main/java/com/finalproject/finsera/finsera/dto/virtualAccount/transferVirtualAccount/TransferVirtualAccountRequestDto.java
+++ src/main/java/com/finalproject/finsera/finsera/dto/virtualAccount/transferVirtualAccount/TransferVirtualAccountRequestDto.java
@@ -1,9 +1,7 @@
 package com.finalproject.finsera.finsera.dto.virtualAccount.transferVirtualAccount;
 
-import com.finalproject.finsera.finsera.model.enums.AccountType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
-
 @Data
 public class TransferVirtualAccountRequestDto {
     @Schema(name = "virtualAccountNumber", example = "9876543")
