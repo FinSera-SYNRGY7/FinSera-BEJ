c22eb0276ef4142f9bf95b2fde260d9a96298800
WINDOWS <fahrizalshofyanaziz@gmail.com>
2024-08-23 21:42:06 +0700

    fix :rerun

--- src/main/java/com/finalproject/finsera/finsera/dto/transaction/TransactionCheckAccountRequestDto.java
+++ src/main/java/com/finalproject/finsera/finsera/dto/transaction/TransactionCheckAccountRequestDto.java
@@ -7,4 +7,5 @@ import lombok.Data;
 public class TransactionCheckAccountRequestDto {
     @Schema(name = "accountnum_recipient", example = "987654321")
     private String accountnum_recipient;
+
 }
